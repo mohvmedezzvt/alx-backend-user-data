5fc88c0975f67c42496dc1c127517e9e398af83a
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-12 16:11:55 +0200

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-Regex-ing
+Filters sensitive data in a message.
 """
 
 import re
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     Returns the log message obfuscated.
     """
     for field in fields:
-        message = re.sub(rf'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}', message)
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
     return message
