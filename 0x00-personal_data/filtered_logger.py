bba68210551b25e5965470d8defe037c61b106e9
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-12 15:05:23 +0200

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Returns the log message obfuscated.
+    """
+    for field in fields:
+        message = re.sub(rf'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
