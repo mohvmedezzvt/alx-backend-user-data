53bc16965bd910e50afaef6d12e7579bb7b1cbc1
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-18 23:05:52 +0200

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header method"""
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method"""
