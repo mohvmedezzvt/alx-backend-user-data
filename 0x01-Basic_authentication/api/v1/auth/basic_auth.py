8d1912d893c02418b34d0f2595c305c85382fc2f
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-19 18:46:58 +0200

    Basic - Allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,7 +56,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.rsplit(':', 1)
+        credentials = decoded_base64_authorization_header.split(':', 1)
         if len(credentials) == 1:
             return (credentials[0], None)
 
