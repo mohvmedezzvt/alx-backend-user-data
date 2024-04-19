86971c0876eb2161de9bd132965fa2abddee77e5
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-19 18:50:47 +0200

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,20 +8,16 @@ from typing import List, TypeVar
 class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth method"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """Check if a path requires authentication."""
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-
-        if path.endswith('/'):
-            path = path[:-1]
-
+        
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('/'):
-                excluded_path = excluded_path[:-1]
-
-            if path == excluded_path:
+            if excluded_path.endswith("*") and path.startswith(excluded_path[:-1]):
                 return False
-
+            elif path == excluded_path:
+                return False
+                
         return True
 
     def authorization_header(self, request=None) -> str:
