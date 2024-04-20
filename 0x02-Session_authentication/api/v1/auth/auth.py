b897c25e737ed7155368e0d7b165fadd57ffa0e4
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-20 12:55:04 +0200

    Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Auth module"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if a path requires authentication."""
+        if bool(path and excluded_paths):
+            path = path + '/' if path[-1] != '/' else path
+            for excluded_path in excluded_paths:
+                if excluded_path.endswith('*') and path.startswith(
+                   excluded_path[:-1]):
+                    return False
+                if excluded_path == path:
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header method"""
+        if bool(request and 'Authorization' in request.headers.keys()):
+            return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user method"""
+        return None
