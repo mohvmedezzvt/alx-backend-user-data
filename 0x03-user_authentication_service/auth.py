83ea5e55c7fbec0a3cbd43eab952391eed80eb8c
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-26 20:15:05 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,40 @@
 """Auth module"""
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+
+        Args:
+            email (str): The email of the user
+            password (str): The password of the user
+
+        Returns:
+            User: The User object
+
+        Raises:
+            ValueError: If a user with the provided email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+        return user
 
 
 def _hash_password(password: str) -> bytes:
