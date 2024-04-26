e08bf05b333bec84e3a366933f13e0c313ab0d9f
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-26 20:20:17 +0300

    Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Main file
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    """Index
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
