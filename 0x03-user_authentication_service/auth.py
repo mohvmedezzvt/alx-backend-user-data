b4c58df554e442630f135cb6c2dbc39d7be0deb8
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-26 21:13:29 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,6 +57,23 @@ class Auth:
         except ValueError:
             return False
 
+    def create_session(self, email: str) -> str:
+        """Create a session
+
+        Args:
+            email (str): The email of the user
+
+        Returns:
+            str: The session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
